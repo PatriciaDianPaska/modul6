f1959f3aba79d48224be9c9e9d6d75b34b6338a0
PatriciaDianPaska <patricia_dian_26rpl@student.smktelkom-mlg.sch.id>
2019-01-12 12:21:31 +0700

    first commit

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['tb_siswa'])) {
+      header("location: form-login.php");
+    }
+    session_destroy();
+
+    header("location: form-login.php");
+
+
+?>
---
+++ record.php
@@ -0,0 +1,52 @@
+<?php
+require_once "./connect.php";
+
+$sql = 'SELECT * FROM tb_siswa';
+$result = mysqli_query($connect, $sql);
+if ($result) {
+  if (mysqli_num_rows($result)) {
+    ?>
+    <table border= 1 cellspascing =1 cellpadding=5>
+       <tr>
+         <td>#</td>
+         <td widht=100>NIS</td>
+         <td widht=150>NAMA</td>
+         <td>Alamat</td>
+         <td>Password</td>
+       </tr>
+      <?php
+         $i = 1;
+         while ($row = mysqli_fetch_row($result)) {
+           ?>
+           <tr>
+             <td>
+               <?php echo $i;?>
+             </td>
+             <td>
+               <?php echo $row[0]; ?>
+             </td>
+             <td>
+               <?php echo $row[1]; ?>
+             </td>
+             <td>
+               <?php echo $row[2]; ?>
+             </td>
+             <td>
+               <?php echo $row[3]; ?>
+             </td>
+           </tr>
+           <?php
+             $i++;
+         }
+       ?>
+    </table>
+       <?php
+  }
+
+
+    else {
+        echo "Data tidak ditemukan";
+    }
+  mysqli_close($connect);
+}
+     ?>
